7563a88e88ead023c5e253769560328806aa5ab7
SteamMachina <52200491+SteamMachina@users.noreply.github.com>
2021-02-20 16:58:38 +0100

    Update exercice1.py

--- exercices/exercice1.py
+++ exercices/exercice1.py
@@ -1,2 +1,16 @@
 def rechercheMinMax(tab):
-    return ...
+  dico = {}
+  if tab != []:
+    min = tab[0]
+    max = tab[0]
+    for element in tab:
+      if element < min:
+        min = element
+      elif element > max:
+        max = element
+    dico["min"] = min
+    dico["max"] = max
+  else :
+    dico["min"] = None
+    dico["max"] = None
+  return dico
